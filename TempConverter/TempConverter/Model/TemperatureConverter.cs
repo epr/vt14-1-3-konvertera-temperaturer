6b43f8dd78315635448a109ac2a9a75013c2988b
Eddy Proca <eprcz09@student.lnu.se>
2014-02-04 22:08:46 +0100

    wrote temperature converter class

---
+++ TempConverter/TempConverter/Model/TemperatureConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TempConverter.Model
+{
+    public static class TemperatureConverter
+    {
+        public static int CelsiusToFahrenheit(int degreesC)
+        {
+            return (int)(degreesC * 1.8 + 32);
+        }
+        public static int FahrenheitToCelsius(int degreesF)
+        {
+            return (int)((degreesF - 32) / 1.8);
+        }
+    }
+}
